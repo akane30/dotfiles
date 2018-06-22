525de48e5a2baffc2a4fb95d703cce62129a74ff
Aaron Kane <akane@ecr.ai>
2018-06-22 15:34:29 -0400

    Updating to bare-repo dotfile management
    Changing the file management structure to the external bare repo style in accordance with https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/

---
+++ .bin/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+git clone --bare https://github.com/akane30/dotfiles.git "$HOME/.cfg"
+
+function dfconfig {
+	`which git` --git-dir="$HOME/.cfg/" --work-tree="$HOME" $@
+}
+
+mkdir -p "${HOME}/.cfg-backup"
+
+dfconfig checkout
+
+if [ $? = 0]; then
+	echo "Checked out config."
+else
+	echo "Backing up pre-existing dot files..."
+	dfconfig checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} "${home}/.cfg-backup/{}"
+	# checkout again now that conflicting files removed
+	dfconfig checkout
+fi
+
+# set no showing untracked files
+dfconfig config status.showUntrackedFiles no
